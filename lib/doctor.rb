6c79b104785575dd07a1205e8d988fec1735caa8
Brodie Curtis <curtis.brodie1011@yahoo.com>
2020-07-18 19:43:00 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,35 @@
+class Doctor
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(date, patient, self)
+  end
+
+  def appointments
+    Appointment.all.select { |appointment| appointment.doctor == self }
+  end
+
+  def patients
+    appointments.map {|appointment| appointment.patient}
+  end
+end
+
+
+# describe '#patients' do
+#     it 'has many patients, through appointments' do
+#       doctor_who = Doctor.new('The Doctor')
+#       hevydevy = Patient.new('Devin Townsend')
+#       doctor_who.new_appointment('Friday, January 32nd', hevydevy)
+
+#       expect(doctor_who.patients).to include(hevydevy)
