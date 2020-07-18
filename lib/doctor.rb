2b427e1573c254603bbc7e39127efcb1b32b3cee
Brodie Curtis <curtis.brodie1011@yahoo.com>
2020-07-18 19:47:15 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -21,23 +21,11 @@ class Doctor
   end
 
   def patients
-    appointments.map do |appointment|
+    appointments.collect do |appointment|
       appointment.patient
     end
   end
 end
 
 
-# describe '#patients' do
-#     it 'has many patients, through appointments' do
-#       doctor_who = Doctor.new('The Doctor')
-#       hevydevy = Patient.new('Devin Townsend')
-#       doctor_who.new_appointment('Friday, January 32nd', hevydevy)
 
-#       expect(doctor_who.patients).to include(hevydevy)
-# def genres
-#     songs.map do |song|
-#       song.genre
-#     end
-#   end
-# end
